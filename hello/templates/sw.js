f3969fa9b6e7c85930554ad746230485db9398b4
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-10-26 00:51:52 +0530

    install functionality

--- hello/templates/sw.js
+++ hello/templates/sw.js
@@ -1,6 +1,16 @@
-const VERSION = '1.0.2';
+const VERSION = '1.0.6';
 
+// Listen to install event
 self.addEventListener('install', (event) => {
-    console.log('beep');
-    console.log('[SW] Installing SW version:', VERSION);
+    console.log('[SW] Service worker installed. ver=' + VERSION);
+});
+
+// Listen to activate event
+self.addEventListener('activate', (event) => {
+    console.log("[SW] Service worker activated")
+});
+
+// Listen to fetch event
+self.addEventListener('fetch', (event) => {
+    console.log("[SW] Fetch event", event)
 });
